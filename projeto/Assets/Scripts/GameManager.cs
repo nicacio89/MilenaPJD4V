a58e3bbcc4bd46dc9431766c10cb6782b866fb3a
nicacio89 <nicacio.m@escolar.ifrn.edu.br>
2023-04-03 17:47:57 -0300

    aula3deabril

--- projeto/Assets/Scripts/GameManager.cs
+++ projeto/Assets/Scripts/GameManager.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadScene("MainMenu");
     }
 
     // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
---
+++ projeto/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public string SceneName;
+
+    public void LoadScene()
+    {
+        GameManager.Instance.LoadScene(SceneName);
+    }
+}
+
+   
